cb5fe8754ae6e218167393fce5a68e7e21e9e69c
404Nikhil <nicksd111@gmail.com>
2023-08-25 23:30:59 +0530

    initial_push

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],  
+  theme: {
+    extend: {
+    //   animation: {
+    //     slide: {
+    //     '0%': { transform: 'translateX(0' },
+    //     '100%': { transform: 'translateX(-100%)' },
+    //       }
+    // },
+  },
+  plugins: [],
+}
+}
