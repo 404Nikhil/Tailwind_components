a5fa35991a0adae06e07259b0a27fcec4cc2fc67
404Nikhil <nicksd111@gmail.com>
2023-08-27 20:04:02 +0530

    navbar_component_final

--- src/csl.js
+++ src/csl.js
@@ -1,17 +1,29 @@
-var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    loop: true, // Enable infinite looping
-  });
+function toggleMenu(flag) {
+  let value = document.getElementById("menu");
+  if (flag) {
+      value.classList.remove("hidden");
+  } else {
+      value.classList.add("hidden");
+  }
+}
+
+
+// var swiper = new Swiper(".mySwiper", {
+//     effect: "coverflow",
+//     grabCursor: true,
+//     centeredSlides: true,
+//     slidesPerView: "auto",
+//     coverflowEffect: {
+//       rotate: 50,
+//       stretch: 0,
+//       depth: 100,
+//       modifier: 1,
+//       slideShadows: true,
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//     },
+//     loop: true, 
+//   });
+
+
